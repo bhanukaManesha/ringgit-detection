26dd7954ab2a302495ae2f0ac535f06fe38f52af
Bhanuka Gamage <bhanukamgamage@gmail.com>
2019-12-26 16:14:53 +0800

    Initial Commit

---
+++ train.py
@@ -0,0 +1,16 @@
+
+import torch.nn as nn
+
+class YOLO (nn.) :
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+
+    def forward(self, input):
+
+        SEED = 32
+
+
+        for i in range(0, int(math.log(GRID_X/WIDTH), 0.5)):
+            
+
+

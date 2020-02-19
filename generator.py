81901b16776f01df03b559434a332ff575e52282
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-02-19 19:03:38 +0800

    Refactor the generator

--- generator.py
+++ generator.py
@@ -1,278 +1,143 @@
 #!/usr/bin/env python3
-from common import *
-from utils import *
 from argparse import ArgumentParser
 from tqdm import tqdm
 from copy import deepcopy
+import math, os, random
+import numpy as np
 
-def generate_geometrical_noise(image):
-    height, width, depth = image.shape
 
-    # # Draw line.
-    for _ in range(50):
-        x1 = random.randint(0, width)
-        y1 = random.randint(0, height)
-        x2 = random.randint(0, width)
-        y2 = random.randint(0, height)
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        thickness = random.randint(1, 5)
+from common import *
+import aug
+from data import Data
+from render import Render
 
-        image = cv2.line(image, (x1,y1), (x2,y2), (r,g,b), thickness)
+class Generator:
 
-    # Draw rect.
-    for _ in range(50):
-        x1 = random.randint(0, width)
-        y1 = random.randint(0, height)
-        x2 = random.randint(0, width)
-        y2 = random.randint(0, height)
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        thickness = random.randint(1, 3)
+    def generate_polygon_from_raw(self, x_images,y_polygons,no_images = 1):
+        '''
+        main function to generete the images
+        @rows - number of rows for the image
+        @col - number of columns for the image
+        '''
+        allpolygons = []
 
-        image = cv2.rectangle(image, (x1,y1), (x2,y2), (r,g,b), thickness)
+        # Generate the background
+        background = aug.generate_background()
 
-    # Draw circle.
-    for _ in range(200):
-        x1 = random.randint(-width//2, width+width//2)
-        y1 = random.randint(-height//2, height+height//2)
-        radius = random.randint(0, width)
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        thickness = random.randint(1, 3)
+        for _ in range(no_images):
 
-        image = cv2.circle(image, (x1,y1), radius, (r,g,b), thickness)
+            output_currency = random.choice(CLASSES)
+            output_currency_index = CLASS[output_currency]
 
-    return image
+            # Get random image and respective set of points
+            rand_int = random.randint(0,len(x_images[output_currency_index]) - 1)
 
-def change_brightness(image, mode = "uniform"):
+            image = x_images[CLASS[output_currency]][rand_int]
+            points = y_polygons[CLASS[output_currency]][rand_int]
 
-    if mode == "uniform":
-        image = image * random.uniform(0.5, 1.5)
-        return np.clip(image,a_min = 0, a_max = 255.0)
+            rotate_height, rotate_width, _ = image.shape
 
-    if mode == "transparent_triangle":
+            # Calculating the height and width
+            height, width, channels = background.shape
 
-        pt1 = (random.randint(0,WIDTH), random.randint(0,HEIGHT))
-        pt2 = (random.randint(0,WIDTH), random.randint(0,HEIGHT))
-        pt3 = (random.randint(0,WIDTH), random.randint(0,HEIGHT))
+            random_size = random.uniform(0.6, 0.75)
+            height_of_note = int(math.floor(height * random_size))
+            width_of_note = int(math.floor(width * random_size))
 
-        triangle_cnt = np.array( [pt1, pt2, pt3] )
-        shape = cv2.drawContours(np.full((HEIGHT,WIDTH,CHANNEL), 255.), [triangle_cnt], 0, (0,0,0), -1)
+            # Calculate the x and y
+            x_center = width * random.uniform(0.3, 0.7)
+            y_center = height * random.uniform(0.3, 0.7)
 
-        return cv2.addWeighted(shape,0.3,image,0.7,0)
+            # Resize the image
+            if rotate_height > rotate_width:
+                resize_image = aug.image_resize(image, height=height_of_note)
+            else:
+                resize_image = aug.image_resize(image, width=width_of_note)
 
-def generate_background(mode = "noise"):
+            rheight, rwidth, rchannel = resize_image.shape
+            oheight, owidth, _ = image.shape
 
-    if mode == "white" :
+            # Calculate the top left x and y
+            x_top = abs(int(x_center - (rwidth // 2)))
+            y_top = abs(int(y_center - (rheight // 2)))
 
-        return np.full((RHEIGHT,RWIDTH,CHANNEL), 255.)
+            for i, [x,y] in enumerate(points):
+                nx = (x / owidth) * rwidth
+                ny = (y / oheight) * rheight
 
-    elif mode== "black" :
+                points[i] = [nx + x_top,ny + y_top]
 
-        return np.full((RHEIGHT,RWIDTH,CHANNEL), 0.)
+            # Overlay the image to the background image
+            final_image = aug.overlay_transparent(background,resize_image,x_top,y_top)
 
-    elif mode == "noise" :
-        return np.random.randint(256.0, size=(RHEIGHT, RWIDTH,CHANNEL), dtype = np.uint8)
+            polygon = {
+                'confidence' : 1.0,
+                'points':points,
+                'class' : CLASS[output_currency]
+                }
 
-    elif mode == 'geometric':
+            allpolygons.append(polygon)
 
-        return generate_geometrical_noise(np.full((RHEIGHT,RWIDTH,CHANNEL), 1., dtype = np.uint8))
+        return final_image, allpolygons
 
-def generate_polygon(x_images,y_polygons,no_images = 1):
-    '''
-    main function to generete the images
-    @rows - number of rows for the image
-    @col - number of columns for the image
-    '''
+    def serve(self,batch_size):
 
-    allpolygons = []
+        x_images = []
+        y_polygons = []
 
-    # Generate the background
-    background = generate_background()
+        for aclass in CLASSES:
 
-    for _ in range(no_images):
+            # Fix this asap
+            data = Data()
 
-        output_currency = random.choice(CLASSES)
-        output_currency_index = CLASS[output_currency]
+            x, y = data.read_polygons('{}/{}'.format('data/raw_notes',aclass))
+            x_images.append(x)
+            y_polygons.append(y)
 
-        # Get random image and respective set of points
-        rand_int = random.randint(0,len(x_images[output_currency_index]) - 1)
+        while True:
+            # Empty batch arrays.
+            x_trains = []
+            y_trains = []
+            # Create batch data.
+            for i in tqdm(range(batch_size)):
 
-        image = x_images[CLASS[output_currency]][rand_int]
-        points = y_polygons[CLASS[output_currency]][rand_int]
+                image, polygons = self.generate_polygon_from_raw(deepcopy(x_images), deepcopy(y_polygons))
+                image , polygons = aug.augmentation(image, polygons)
+                image, labels = data.convert_render_to_data(image, polygons)
 
-        rotate_height, rotate_width, _ = image.shape
+                # Append
+                x_trains.append(image)
+                y_trains.append(labels)
 
-        # Calculating the height and width
-        height, width, channels = background.shape
+            x_trains = np.asarray(x_trains).reshape((batch_size, HEIGHT, WIDTH, CHANNEL))
+            y_trains = np.asarray(y_trains).reshape((batch_size, GRID_Y, GRID_X, 5+len(CLASSES)))
+            yield x_trains, y_trains
 
-        random_size = random.uniform(0.6, 0.75)
-        height_of_note = int(math.floor(height * random_size))
-        width_of_note = int(math.floor(width * random_size))
-
-        # Calculate the x and y
-        x_center = width * random.uniform(0.3, 0.7)
-        y_center = height * random.uniform(0.3, 0.7)
-
-        # Resize the image
-        if rotate_height > rotate_width:
-            resize_image = image_resize(image, height=height_of_note)
-        else:
-            resize_image = image_resize(image, width=width_of_note)
-
-        rheight, rwidth, rchannel = resize_image.shape
-        oheight, owidth, _ = image.shape
-
-        # Calculate the top left x and y
-        x_top = abs(int(x_center - (rwidth // 2)))
-        y_top = abs(int(y_center - (rheight // 2)))
-
-        for i, [x,y] in enumerate(points):
-            nx = (x / owidth) * rwidth
-            ny = (y / oheight) * rheight
-
-            points[i] = [nx + x_top,ny + y_top]
-
-        # Overlay the image to the background image
-        final_image = overlay_transparent(background,resize_image,x_top,y_top)
-
-        polygon = {
-            'confidence' : 1.0,
-            'points':points,
-            'class' : CLASS[output_currency]
-            }
-
-        allpolygons.append(polygon)
-
-    return final_image, allpolygons
-
-def generate(images) :
-    '''
-    main function to generete the images
-    @rows - number of rows for the image
-    @col - number of columns for the image
-    '''
-
-    output_currency=random.choice(CLASSES)
-
-    image = random.choice(images[CLASS[output_currency]])
-
-    # Generate the background
-    background = generate_background()
-
-    # Rotate the image
-    # rimage = rotate_image(image, angle)
-    rotate_height, rotate_width, _ = image.shape
-
-    # Calculating the height and width
-    height, width, channels = background.shape
-
-    random_size = random.uniform(0.6, 0.75)
-    height_of_note = int(math.floor(height * random_size))
-    width_of_note = int(math.floor(width * random_size))
-
-    # Calculate the x and y
-    x_center = width * random.uniform(0.3, 0.7)
-    y_center = height * random.uniform(0.3, 0.7)
-
-    # Resize the image
-    if rotate_height > rotate_width:
-        resize_image = image_resize(image, height=height_of_note)
-    else:
-        resize_image = image_resize(image, width=width_of_note)
-
-    rheight, rwidth, rchannel = resize_image.shape
-
-    # Calculate the top left x and y
-    x_top = abs(int(x_center - (rwidth // 2)))
-    y_top = abs(int(y_center - (rheight // 2)))
-
-    # Overlay the image to the background image
-    final_image = overlay_transparent(background,resize_image,x_top,y_top)
-
-    # Change Brightness
-    # final_image = change_brightness(final_image)
-
-    polygon = {
-        'confidence' : 1.0,
-        'points':[
-            [x_top,y_top],
-            [x_top + rwidth, y_top],
-            [x_top + rwidth, y_top + rheight],
-            [x_top, y_top + rheight]
-        ],
-        'class' : CLASS[output_currency]
-        }
-
-    return final_image, [polygon]
-
-def generator(batch_size):
-
-    x_images = []
-    y_polygons = []
-
-    for aclass in CLASSES:
-        x, y = read_polygons('{}/{}'.format(money_path,aclass))
-        x_images.append(x)
-        y_polygons.append(y)
-
-
-    while True:
-        # Empty batch arrays.
-        x_trains = []
-        y_trains = []
-        # Create batch data.
-        for i in tqdm(range(batch_size)):
-
-            image, polygons = generate_polygon(deepcopy(x_images), deepcopy(y_polygons))
-            image , polygons = augmentation(image, polygons)
-            image, labels = convert_data_to_yolo(image, polygons)
-
-            # Append
-            x_trains.append(image)
-            y_trains.append(labels)
-
-        x_trains = np.asarray(x_trains).reshape((batch_size, HEIGHT, WIDTH, CHANNEL))
-        y_trains = np.asarray(y_trains).reshape((batch_size, GRID_Y, GRID_X, 5+len(CLASSES)))
-        yield x_trains, y_trains
 
 if __name__ == "__main__" :
     # parse arguments
     parser = ArgumentParser()
     parser.add_argument("-c", "-count", dest="count",
                         help="number of training data", metavar="file")
-    parser.add_argument("-r", "-render", dest="render",
+    parser.add_argument("-r", "-render", dest="render", default= False,
                         help="render each image")
     args = parser.parse_args()
 
     if args.count == None and int(args.count) > 50:
         print("-c enter a number greater than 50")
 
-    if args.render == None:
-        render = False
-    else:
-        render = True
+    render = True if args.render else False
 
     COUNT = int(args.count)
 
-    x_train,y_train = next(generator(COUNT))
+    # Create a generator
+    generator = Generator()
+    x_train,y_train = next(generator.serve(COUNT))
 
-    # Create a folder
-    directory = "pickles"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    write_pickle_datas('pickles/trains.pickle', datas=(x_train, y_train))
+    # Fix this asap
+    data = Data()
+    data.write_pickle_datas(datas=(x_train, y_train))
 
     if render :
-        for i in range(50):
-            x_data = x_train[i]
-            y_data = y_train[i]
-
-            image, label = convert_data_to_image(x_data, y_data)
-            rendered = render_with_labels(image, label, display = False)
-            cv2.imwrite('output_render/test_render_{:02d}.png'.format(i),rendered)
+        r = Render()
+        r.output_result(x_train, y_train, folder= 'output_render')

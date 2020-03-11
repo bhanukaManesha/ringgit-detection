fd3369000e4415b211ab449c8aa5e1abbe71ebb9
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-03-11 13:46:43 +0800

    Remove augmentation

--- lib/data/AugmentData.py
+++ lib/data/AugmentData.py
@@ -15,9 +15,9 @@ class AugmentData(Data):
     def __init__(self, data, dtype):
         super().__init__(data, dtype)
         self.augmentationseq = iaa.Sequential([
-            iaa.ChangeColorTemperature((4000, 9000)),
+            # iaa.ChangeColorTemperature((4000, 9000)),
             iaa.Rotate(rotate=(0, 359)),
-            iaa.Affine(scale=(0.8, 1.2)),
+            # iaa.Affine(scale=(0.8, 1.2)),
             # iaa.ElasticTransformation(alpha=(0, 5.0), sigma=0.5),
             # iaa.PerspectiveTransform(scale=(0.02, 0.08), keep_size=True),
             iaa.Resize({'width': WIDTH, 'height': HEIGHT}, interpolation=imgaug.ALL)

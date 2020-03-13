39a5c1af11393cb8f91f8fdeb92128fcf8e4ff1d
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-03-13 16:40:08 +0800

    Add Contrast to augmentation

--- lib/data/AugmentData.py
+++ lib/data/AugmentData.py
@@ -15,7 +15,8 @@ class AugmentData(Data):
     def __init__(self, data, dtype):
         super().__init__(data, dtype)
         self.augmentationseq = iaa.Sequential([
-            # iaa.ChangeColorTemperature((4000, 9000)),
+            iaa.ChangeColorTemperature((4000, 9000)),
+            iaa.GammaContrast((0.5, 2.0)),
             iaa.Rotate(rotate=(0, 359)),
             # iaa.Affine(scale=(0.8, 1.2)),
             # iaa.ElasticTransformation(alpha=(0, 5.0), sigma=0.5),

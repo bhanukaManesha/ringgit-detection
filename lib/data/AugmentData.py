9fd458cc870e2f2503511cecbb543fcd9f45dff6
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-03-20 12:19:33 +0800

    add separate augmentation for validation data

--- lib/data/AugmentData.py
+++ lib/data/AugmentData.py
@@ -15,19 +15,53 @@ class AugmentData(Data):
     def __init__(self, data, dtype):
         super().__init__(data, dtype)
         self.augmentationseq = iaa.Sequential([
-            # iaa.ChangeColorTemperature((4000, 9000)),
-            # iaa.GammaContrast((0.5, 2.0)),
             iaa.Rotate(rotate=(0, 359)),
-            # iaa.Rot90((1, 3)),
-            # iaa.Affine(scale=(0.8, 1.2)),
-            # iaa.ElasticTransformation(alpha=(0, 5.0), sigma=0.5),
-            # iaa.PerspectiveTransform(scale=(0.02, 0.08), keep_size=True),
+            iaa.ChangeColorTemperature((3000, 10000)),
+            iaa.Affine(scale=(0.8, 1.2)),
+            iaa.GammaContrast((0.8, 1.2)),
+            iaa.PerspectiveTransform(scale=(0.02, 0.08), keep_size=True),
+            iaa.Resize({'width': WIDTH, 'height': HEIGHT}, interpolation=imgaug.ALL)
+        ])
+
+        self.resizeseq = iaa.Sequential([
             iaa.Resize({'width': WIDTH, 'height': HEIGHT}, interpolation=imgaug.ALL)
         ])
 
     @classmethod
     def fromdataobj(cls, dataobj):
         return cls(data=(dataobj.x, dataobj.y), dtype=dataobj.dtype)
+
+    def resize(self):
+        polygons = []
+        for polygon in self.y:
+            polygons.append(Polygon(polygon['points']))
+            polygon['points'] = []
+
+        pps = PolygonsOnImage(polygons, shape=self.x.shape)
+
+        # Augment.
+        self.x, pps = self.resizeseq(image=self.x, polygons=pps)
+        # self.x = cv2.cvtColor(self.x,cv2.COLOR_RGB2GRAY)
+
+        # print("{}".format(self.x.shape))
+
+        for i,polygon in enumerate(self.y):
+
+            for (x,y) in pps[i]:
+
+                if x <= 0:
+                    x = 0
+                if y <= 0:
+                    y = 0
+                if x >= WIDTH:
+                    x = WIDTH - 1
+                if y >= HEIGHT:
+                    y = HEIGHT - 1
+
+                polygon['points'].append([x,y])
+
+
+
 
     def augmentation(self):
 

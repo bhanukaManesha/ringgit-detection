7b08f987f95c425c7c912ac4763c703f0f43ea71
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-04-03 03:47:08 +0800

    house keeping

--- common.py
+++ common.py
@@ -1,14 +1,13 @@
 
-CLASSES       = ["RM50","RM100"]
+CLASSES       = ["RM10"]
 CLASS = {
-    "RM50" : 0,
-    "RM100" : 1
+    "RM10" : 0
 }
 
 CHANNEL     = 3
 
-GRID_WIDTH  = int(135) # The size of 1 cell.
-GRID_HEIGHT = int(135)
+GRID_WIDTH  = int(154) # The size of 1 cell.
+GRID_HEIGHT = int(154)
 WIDTH       = int(1080)
 HEIGHT      = int(1080)
 GRID_X      = WIDTH // GRID_WIDTH # The number of cells.
